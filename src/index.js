132fea4d33f8cf1a61ed907b9982829b5019f705
Joshua Walker <joshwalk@umich.edu>
2021-11-04 01:41:16 -0400

    Updated handling of components + styling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App post="Aging Like Winez" />
   </React.StrictMode>,
   document.getElementById("root")
 )
